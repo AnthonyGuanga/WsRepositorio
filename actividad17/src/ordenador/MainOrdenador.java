2f7538b86d855f9c6236a5b9e6f2a9c491ef68d5
AnthonyGu <ninfrad23@gmail.com>
2024-02-27 14:21:45 +0100

    add act 17 sin terminar

--- actividad17/src/ordenador/MainOrdenador.java
+++ actividad17/src/ordenador/MainOrdenador.java
@@ -4,15 +4,15 @@ public class MainOrdenador {
 
 	public static void main(String[] args) {
 		System.out.println("");
-		Procesador p1 = new Procesador();
-		TarjetaGrafica t1 = new TarjetaGrafica();
-		PlacaBase pb1 = new PlacaBase();
-		RAM r1 = new RAM();
-		RAM r2 = new RAM();
-		RAM r3 = new RAM();
-		Periferico pr1 = new Periferico();
-		Periferico pr2 = new Periferico();
-		Periferico pr3 = new Periferico();
+		Procesador p1 = new Procesador("intel", "i5", 5 ,100);
+		TarjetaGrafica t1 = new TarjetaGrafica("nvidea","rtx4090",27,1.500,);
+		PlacaBase pb1 = new PlacaBase("msi",65,"b550");
+		RAM r1 = new RAM("hyperx",8,65);
+		RAM r2 = new RAM("samsumg",16,105);
+		RAM r3 = new RAM("corsair",8,65);
+		Periferico pr1 = new Periferico("mouse","logitech",86);
+		Periferico pr2 = new Periferico("teclado","corsair",110);
+		Periferico pr3 = new Periferico("headset","lenovo",200);
 
 		Ordenador o = new Ordenador();
 		
